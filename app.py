647724034c9f5a374bfd33c1e80c161183a3da98
Rasmus Krogsgaard Vester <124626087+Trlton@users.noreply.github.com>
2026-04-08 14:44:13 +0200

    Ændret til at køre på flask. (Nemmere retrieve to database stuff)

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-import webbrowser, os
+from flask import *
 
-webbrowser.open(f"file://{os.path.abspath('UI/home.html')}")
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return send_from_directory("UI", "home.html")
+
+@app.route("/<path:htmlFil>")
+def static_files(htmlFil):
+    return send_from_directory("UI", htmlFil)
+
+if __name__ == "__main__":
+    import webbrowser
+    webbrowser.open("http://localhost:5000")
+    app.run()
